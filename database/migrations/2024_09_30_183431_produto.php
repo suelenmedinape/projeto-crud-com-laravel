69b761f4414c4dda3f031b0691f908bb28e6be27
suelenmedinape <suelenmedinape@gmail.com>
2024-09-30 15:53:13 -0300

    primeiro commit

---
+++ database/migrations/2024_09_30_183431_produto.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao', 255);
+            $table->enum('categoria', ['ALIMENTACAO', 'BEBIDAS', 'HIGIENE', 'LIMPEZA', 'OUTROS'])->default('OUTROS');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(table: 'produto');
+    }
+};
