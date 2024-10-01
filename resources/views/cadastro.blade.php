7f4165da7fe66dc1bc8639d1702e9d36314f1329
suelenmedinape <suelenmedinape@gmail.com>
2024-10-01 20:36:22 -0300

    att tela cadastro

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -7,21 +7,23 @@
 
             </x-nav>
             </x-slot>
-            <form method="post" action="/add">
-                @csrf
-                <label for="descricao"> Descrição: </label>
-                <input type="text" name="descricao" id="desc" required><br>
-                <label for="categoria"> Categoria do Produto: </label>
-                <select name="categoria" id="categ" required>
-                    <option value="categoria" disabled selected>SELECIONE UMA CATEGORIA</option>
-                    <option value="alimentacao">ALIMENTAÇÃO</option>
-                    <option value="bebidas">BEBIDAS</option>
-                    <option value="higiene">HIGIENE</option>
-                    <option value="limpeza">LIMPEZA</option>
-                    <option value="outros">OUTROS</option>
-                </select><br>
-                <label for="quantidade"> Quantidade: </label>
-                <input type="number" name="quantidade" id="quant" required><br>
-                <input type="submit" class="btn" value="Cadastrar">
-            </form>
+            <div class="form">
+                <form method="post" action="/add">
+                    @csrf
+                    <label for="descricao"> Descrição: </label>
+                    <input type="text" name="descricao" id="desc" required>
+                    <label for="categoria"> Categoria do Produto: </label>
+                    <select name="categoria" id="categ" required>
+                        <option value="categoria" disabled selected>SELECIONE UMA CATEGORIA</option>
+                        <option value="alimentacao">ALIMENTAÇÃO</option>
+                        <option value="bebidas">BEBIDAS</option>
+                        <option value="higiene">HIGIENE</option>
+                        <option value="limpeza">LIMPEZA</option>
+                        <option value="outros">OUTROS</option>
+                    </select>
+                    <label for="quantidade"> Quantidade: </label>
+                    <input type="number" name="quantidade" id="quant" required>
+                    <input type="submit" class="btn" value="Cadastrar">
+                </form>
+            </div>
 </x-app>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,6 +1,8 @@
 <header class="container-nav">
+    <div class="logotipo">
+        <a href=""> Produtos </a>
+    </div>
     <nav>
-        <a href="#" class="logo"> Suelen </a>
         <a href="/" class="{{ Request::is('/') ? 'active' : '' }}"> Home </a>
         <a href="/cadastro" class="{{ Request::is('cadastro') ? 'active' : '' }}"> Cadastro </a>
         <a href="/lista" class="{{ Request::is('lista') ? 'active' : '' }}"> Lista </a>
