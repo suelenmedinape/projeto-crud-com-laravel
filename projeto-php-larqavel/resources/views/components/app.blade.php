9fdb91f0d1f695b4b74db3942b22bedfcddc6c13
suelenmedinape <suelenmedinape@gmail.com>
2024-10-08 19:09:13 -0300

    projeto adicionado a uma pasta

--- projeto-php-larqavel/resources/views/components/app.blade.php
+++ projeto-php-larqavel/resources/views/components/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href=" {{ url('/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href=" {{ url('/css/nav.css') }}">
     <link rel="stylesheet" href=" {{ url('/css/style.css') }}">
     <title>Todo App - {{ $title }}</title>
 </head>
